86e9da5f1c6185b9fd46452d353bd98de88977b9
Saqlain Javed <saqlainsj12@gmail.com>
2023-12-15 07:03:51 +0500

    still stuck on ui

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,21 +1,44 @@
 import React from "react";
 import userProfile from "../assets/userProfile.jpg";
 import { AiFillMessage } from "react-icons/ai";
+import { useState } from "react";
 
 const Sidebar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="bg-gray-800 rounded-md w-20 p-3">
-      <div>
+    <div
+      className={`open bg-gray-800 rounded-md origin-left duration-500 ${
+        !open ? "w-20" : "w-60"
+      } px-4 py-4`}
+    >
+      <div
+        onClick={() => setOpen(!open)}
+        className="mb-8  flex flex-col items-center "
+      >
         <img
           className="rounded-full border-2 cursor-pointer"
           src={userProfile}
           alt=""
         />
+        <h3>Saqlain Javed</h3>
       </div>
-      <hr />
-      <div>
-        <AiFillMessage size="2.2rem" />
-        <p>Inbox</p>
+
+      <div className="flex items-center gap-2 cursor-pointer pt-2">
+        <AiFillMessage color="white" size="2.2rem" />
+        <p className={`${!open && "hidden"}`}>Inbox</p>
+      </div>
+      <div className="flex items-center gap-2  pt-2 cursor-pointer">
+        <AiFillMessage color="white" size="2.2rem" />
+        <p className={`${!open && "hidden"}`}>Profile</p>
+      </div>
+      <div className="flex items-center gap-2  pt-2 cursor-pointer">
+        <AiFillMessage color="white" size="2.2rem" />
+        <p className={`${!open && "hidden"}`}>Interactions</p>
+      </div>
+      <div className="flex items-center gap-2  pt-2 cursor-pointer">
+        <AiFillMessage color="white" size="2.2rem" />
+        <p className={`${!open && "hidden"}`}>Something longer</p>
       </div>
     </div>
   );
